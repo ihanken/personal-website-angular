895579bec8a254f3a2c6353f667dbdaffac7fc60
ihanken <ihanken@bellsouth.net>
2018-01-27 10:58:40 -0600

    added travis.

---
+++ src/app/github-card/github-card.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-card',
+  templateUrl: './github-card.component.html',
+  styleUrls: ['./github-card.component.css']
+})
+export class GithubCardComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
